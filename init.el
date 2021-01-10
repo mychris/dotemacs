433a51b0ddeccca55a1b77d3e4b89fd93e522fdb
Christoph Göttschkes <just.mychris@googlemail.com>
2021-01-10 20:13:02 +0100

    Fixes customization.

--- init.el
+++ init.el
@@ -12,9 +12,9 @@
 
 ;;;; Customization information
 
-(let ((custom-file (expand-file-name "custom.el" user-emacs-directory)))
-  (when (file-exists-p custom-file)
-	(load custom-file)))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 ;;;; Bootstrap use-package
 
@@ -84,43 +84,3 @@
 (org-babel-load-file (expand-file-name "settings.org" user-emacs-directory))
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-background nil t)
- '(aw-keys '(97 115 100 102 103 104 106 107 108) t)
- '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "ec5f697561eaf87b1d3b087dd28e61a2fc9860e4c862ea8e6b0b77bd4967d0ba" "57fe2bf84d81baecc6d89ed97bdb19936a3052fc2551ca178667fc45feef2381" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" default))
- '(golden-ratio-scroll-highlight-flag nil t)
- '(highlight-thing-delay-seconds 2)
- '(highlight-thing-exclude-thing-under-point t)
- '(highlight-thing-prefer-active-region t)
- '(ivy-display-style 'fancy)
- '(ivy-use-virtual-buffers t)
- '(lsp-ui-peek-enable t t)
- '(lsp-ui-sideline-enable nil t)
- '(my-background-color "gray9" t)
- '(neo-dont-be-alone t t)
- '(neo-smart-open t)
- '(neo-theme 'icons)
- '(neo-vc-integration '(face char))
- '(neo-window-fixed-size nil)
- '(org-bullets-bullet-list '("◉" "○" "✸" "✿" "☼" "⚬") t)
- '(package-selected-packages
-   '(embark marginalia consult diminish org-superstar magit keycast all-the-icons spinner ggtags spacemacs-theme lsp-ui company-lsp cquery lsp-mode flycheck-pos-tip json-mode yaml-mode markdown-mode delight company-quickhelp toml-mode racer sexy-monochrome expand-region yasnippet-snippets yasnippet counsel ace-window which-key zenburn-theme flycheck-rust flycheck cargo undo-tree rust-mode neotree))
- '(sml/mode-width 'full)
- '(sml/name-width 30)
- '(sml/no-confirm-load-theme t)
- '(sml/shorten-modes t)
- '(sml/theme 'dark)
- '(tab-always-indent 'complete)
- '(undo-tree-auto-save-history t)
- '(undo-tree-visualizer-diff t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
