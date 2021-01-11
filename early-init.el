d70c8feb0e805effaf03cb5bf702e10f3a1859bb
Christoph Göttschkes <just.mychris@googlemail.com>
2021-01-11 21:55:15 +0100

    Moves some initialization into early-init.el.

--- early-init.el
+++ early-init.el
@@ -15,4 +15,9 @@
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
 
+;; Some early customizations
+(setq-default tool-bar-mode -1)  ;; this is too much
+(setq-default menu-bar-mode 1)   ;; for colleagues
+(setq-default scroll-bar-mode 1) ;; keep for colleagues
+
 ;;; early-init.el ends here
