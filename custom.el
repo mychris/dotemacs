adecfdfdec270b5722d22e3a16a62e7a414e95ca
Christoph Göttschkes <just.mychris@googlemail.com>
2022-06-05 09:35:22 +0200

    Use :custom for ace-window configuration.

--- custom.el
+++ custom.el
@@ -8,8 +8,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-background nil nil nil "Customized with use-package ace-window")
- '(aw-keys '(97 115 100 102 103 104 106 107 108) nil nil "Customized with use-package ace-window")
  '(custom-safe-themes
    '("3d2e532b010eeb2f5e09c79f0b3a277bfc268ca91a59cdda7ffd056b868a03bc" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "ec5f697561eaf87b1d3b087dd28e61a2fc9860e4c862ea8e6b0b77bd4967d0ba" "57fe2bf84d81baecc6d89ed97bdb19936a3052fc2551ca178667fc45feef2381" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" default))
  '(golden-ratio-scroll-highlight-flag nil nil nil "Do not highlight current line before/after scroll")
@@ -29,9 +27,4 @@
  '(sml/theme 'dark))
 
 ;;; custom.el ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+
