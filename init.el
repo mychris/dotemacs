938f5c964f2e251177917f8fcd11b5fb634fad3c
Christoph Göttschkes <just.mychris@googlemail.com>
2022-12-04 10:22:15 +0100

    Keep the 'file-name-handler-alist' while requiring the settings.

--- init.el
+++ init.el
@@ -149,10 +149,11 @@ evaluates to a non-nil value."
           (org-babel-tangle-file file-org file-el "emacs-lisp")
           (when do-byte-compile
             (byte-compile-file file-el))))
+      (require 'settings (file-name-sans-extension file-el)))))
       ;; found that one somewhere, shaves off another 200ms during startup.
       ;; no idea if this has any negative side effects, nothing emerged yet.
-      (let ((file-name-handler-alist nil))
-        (require 'settings (file-name-sans-extension file-el))))))
+;;      (let ((file-name-handler-alist nil))
+;;        (require 'settings (file-name-sans-extension file-el))))))
 
 ;; For now, do not byte compile the settings files. For some reason, this
 ;; messes up some of the use-package declaration.
