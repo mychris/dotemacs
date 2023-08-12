f8c6e07e33bb22e2a111e1a5c3aed18cef663213
Christoph Göttschkes <just.mychris@googlemail.com>
2023-08-12 16:43:36 +0200

    Adding :enable-function helpers to +abbrev.el.

--- elisp/+abbrev.el
+++ elisp/+abbrev.el
@@ -31,6 +31,7 @@
 ;; The following hooks are available:
 ;;
 ;;   `+abbrev-cursor-hook'
+;;
 ;; The expanded abbrev should have a '█' character in it.  If so, it will be
 ;; deleted and the cursor will be positioned at its location.
 ;;
@@ -44,12 +45,37 @@
 ;; Calls `yas-expand-snippet' with the expanded abbrev as its argument.  The
 ;; abbrev should expand to a ya-snippet body, which will then be expanded by
 ;; yas again.
+;;
+;;; Mode specific code and global abbrev tables
+;;
+;; When defining a new abbrev table, one can use the `:enable-function' property
+;; to specifiy, if the abbrev table should be considered for the current
+;; expansion.  To make it possible to only enable certain abbrevs only for
+;; source code, the following functions can be used for the mentioned property.
+;;
+;;   `+abbrev-enable-in-code'
+;;
+;; Enable the abbrev table only if the cursor is in a code section.
+;;
+;;   `+abbrev-enable-globally'
+;;
+;; Always enable this abbrev table.
 
 ;;; Code:
 
 (require 'abbrev)
 (require 's)
 (require 'yasnippet nil 'noerror)
+
+(defun +abbrev--point-in-string-p ()
+  "Return non-nil if the point is in a string."
+  (interactive)
+  (nth 3 (syntax-ppss)))
+
+(defun +abbrev--point-in-comment-p ()
+  "Return non-nil if the point is in a comment."
+  (interactive)
+  (nth 4 (syntax-ppss)))
 
 (defun +abbrev-cursor-hook ()
   "Function to run after abbrev expansion.
@@ -67,18 +93,35 @@ position."
 (defun +abbrev-yas-hook ()
   "Function to run after abbrev expansion.
 Runs `yas-expand' to expand the yasnippet."
-  (yas-expand))
+  (when (not (or (+abbrev--point-in-string-p)
+		 (+abbrev--point-in-comment-p)))
+    (yas-expand)))
 
 (put '+abbrev-yas-hook 'no-self-insert t)
 
 (defun +abbrev-yas-inline-hook ()
   "Function to run after abbrev expansion.
 Runs `yas-expand-snippet' using the expanded abbrev as a template."
-  (let ((expansion (buffer-substring-no-properties last-abbrev-location (point))))
-    (yas-expand-snippet expansion last-abbrev-location (point))))
+  (when (not (or (+abbrev--point-in-string-p)
+		 (+abbrev--point-in-comment-p)))
+    (let ((expansion (buffer-substring-no-properties last-abbrev-location (point))))
+      (yas-expand-snippet expansion last-abbrev-location (point)))))
 
 (put '+abbrev-yas-inline-hook 'no-self-insert t)
 
+(defun +abbrev-enable-in-code ()
+  "Return nil, if the cursor is in a comment or in a string.
+Can be used for the `:enable-function' property of `define-abbrev-table'."
+  (let ((syn (syntax-ppss)))
+    ;; 3 == string
+    ;; 4 == comment
+    (not (or (nth 3 syn)
+	     (nth 4 syn)))))
+
+(defun +abbrev-enable-globally ()
+  "Return always t.
+Can be used for the `:enable-function' property of `define-abbrev-table'."
+  t)
 
 
 (provide '+abbrev)
